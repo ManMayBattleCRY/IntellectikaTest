9f1b25456269880cb5782964f2ad51307805e17a
ManMayBattleCRY <manmaybattlecry@gmail.com>
2024-07-31 08:35:21 +0300

    sphere fixing1

--- Assets/Scripts/MeshGeneration/MeshCreator.cs
+++ Assets/Scripts/MeshGeneration/MeshCreator.cs
@@ -2,6 +2,7 @@ using Intellectika.Geometry;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 namespace Intellectika
 {
@@ -47,16 +48,18 @@ namespace Intellectika
             mesh = GetComponent<MeshFilter>().mesh;
             SingleAngle = 360 / PlaneCount;
             center = transform.position;
-            Triangels = new int[(StepX) * (StepY) * PlaneCount * 6 + (StepX) * (StepY) * 12 +99];
-            Vertices = new Vector3[(StepX + 1)*(StepY + 1) * PlaneCount +3 +1000];
-            bottom = new int[(StepX + 1) * (StepY + 1)];
-            top = new int[(StepX + 1) * (StepY+1)];
+            Triangels = new int[(StepX) * (StepY) * PlaneCount * 6 + (StepX) * (StepY) * 12 * PlaneCount];
+            Vertices = new Vector3[(StepX + 1) * (StepY + 1) * PlaneCount + 3 + 1000];
+            bottom = new int[(StepX + 1) * (StepY + 1) * PlaneCount * PlaneCount];
+            top = new int[(StepX + 1) * (StepY + 1) * PlaneCount * PlaneCount];
             planevectors.v01 = new Vector3(0, Height, 0);
+
+
         }
         public virtual void Awake()
         {
-                ObjectInit();
-             //   UpdateMesh(StepX, StepY);
+            ObjectInit();
+            //   UpdateMesh(StepX, StepY);
         }
 
         public void CreateBottom()/// создаёт треугольники для низа
@@ -70,9 +73,9 @@ namespace Intellectika
                 Triangels[TriangleIndex + 1] = center;
                 Triangels[TriangleIndex] = bottom[i + 1];
                 TriangleIndex += 3;
-                
+
             }
-            
+
 
         }/// создаёт треугольники для низа
 
@@ -102,7 +105,7 @@ namespace Intellectika
             Triangels[TriangleIndex + 2] = Triangels[TriangleIndex + 3] = v10;
             Triangels[TriangleIndex + 5] = v11;
             TriangleIndex += 6;
-            } /// создаёт треугольники для Y сетки
+        } /// создаёт треугольники для Y сетки
 
         public void SetFoundationVertices(int currentY)  /// наполняет массивы верха и низа ID"шниками вершин
         {
@@ -127,7 +130,7 @@ namespace Intellectika
 
         public PlaneVectors RecalculatePlaneVectors()
         {
-            PlaneVectors vectors = planevectors; 
+            PlaneVectors vectors = planevectors;
             vectors.v00 = new Vector3(Radius * geometry.cos(CurrentAngleX - SingleAngle / 2), vectors.v00.y, Radius * geometry.sin(CurrentAngleX - SingleAngle / 2));
             vectors.v01 = new Vector3(vectors.v00.x, vectors.v01.y, vectors.v00.z);
             vectors.v10 = new Vector3(Radius * geometry.cos(CurrentAngleX + SingleAngle / 2), vectors.v00.y, Radius * geometry.sin(CurrentAngleX + SingleAngle / 2));
@@ -140,7 +143,10 @@ namespace Intellectika
 
         public void SetPlaneSingle(int CurrentX, int CurrentY)
         {
-            Vector3 v00 = new Vector3(Vector3.Lerp(planevectors.v00, planevectors.v10, (float)(CurrentX) / StepX).x,
+
+
+                
+                Vector3 v00 = new Vector3(Vector3.Lerp(planevectors.v00, planevectors.v10, (float)(CurrentX) / StepX).x,
             Vector3.Lerp(planevectors.v00, planevectors.v01, (float)(CurrentY) / StepY).y,
             Vector3.Lerp(planevectors.v00, planevectors.v10, (float)(CurrentX) / StepX).z);
             Vertices[CurrentX + VerticeIndex] = v00;
@@ -151,7 +157,76 @@ namespace Intellectika
             Vertices[CurrentX + VerticeIndex + 1] = v10;
             Vector3 v11 = new Vector3(v10.x, v01.y, v10.z);
             Vertices[CurrentX + VerticeIndex + StepX + 2] = v11;
+  
+            
+
+
+
         } // Устанавливает значения векторов вершинам в массиве вершин, по сути создаёт единичный квадрат для заполнения треугольниками
+
+
+
+        public void CreateLast()
+        {
+            for (int y = 0; y < StepY; y++)
+            {
+                for (int x = 0; x < StepX - 1; x++)
+                {
+                    SetPlaneSingle(x, y);
+
+                    CreatePlaneTriangels(x);
+
+                }
+
+                SetFoundationVertices(y);
+
+
+                VerticeIndex += StepX + 1;
+            }
+            VerticeIndex += StepX + 1;
+
+
+            for (int y = 0; y < StepY; y++)
+            {
+                //Vector3 v00 = new Vector3(Vector3.Lerp(planevectors.v00, planevectors.v10, (float)(StepX) / StepX).x,
+                //Vector3.Lerp(planevectors.v00, planevectors.v01, (float)(y) / StepY).y,
+                //Vector3.Lerp(planevectors.v00, planevectors.v10, (float)(StepX) / StepX).z);
+                //Vertices[StepX + VerticeIndex] = v00;
+
+
+                //Vector3 v01 = new Vector3(v00.x, Vector3.Lerp(planevectors.v00, planevectors.v01, (float)(y + 1) / StepY).y, v00.z);
+                //Vertices[StepX + VerticeIndex + StepX + 1] = v01;
+
+
+                //Vector3 v10 = Vertices[(StepX + 1) * y];
+                //Vertices[StepX + VerticeIndex + 1] = v10;
+
+
+                //Vector3 v11 = Vertices[(StepX + 1) * y + (StepX + 1)];
+                //Vertices[StepX + VerticeIndex + StepX + 2] = v11;
+
+              //  SetFoundationVertices(y);
+                //CreatePlaneTriangels(StepX + 1);  
+
+
+                int v00 =  VerticeIndex - ((StepX) * (StepY)) + StepX *5 + 1;
+                int v01 =  (StepX + 1) * y;
+                int v10 = VerticeIndex - ((StepX) * (StepY)) + StepX + 1;
+                int v11 = (StepX + 1) * y + (StepX + 1);
+                Triangels[TriangleIndex] = v00;
+                Triangels[TriangleIndex + 1] = Triangels[TriangleIndex + 4] = v01;
+                Triangels[TriangleIndex + 2] = Triangels[TriangleIndex + 3] = v10;
+                Triangels[TriangleIndex + 5] = v11;
+                TriangleIndex += 6;
+
+
+                VerticeIndex += StepX + 1 ;
+            }
+            VerticeIndex += StepX + 1;
+
+
+        }
+
 
         //public PlaneVectors RecalculateSphereVectors()
         //{
@@ -188,10 +263,6 @@ namespace Intellectika
             vectors.v10 = new Vector3(Radius * geometry.cos(CurrentAngleX + SingleAngle / 2), vectors.v00.y, Radius * geometry.sin(CurrentAngleX + SingleAngle / 2));
             vectors.v11 = new Vector3(vectors.v10.x, vectors.v01.y, vectors.v10.z);
 
-            vectors.v00 = vectors.v00.normalized;
-            vectors.v01 = vectors.v01.normalized;
-            vectors.v10 = vectors.v10.normalized;
-            vectors.v11 = vectors.v11.normalized;
 
             CurrentAngleX += SingleAngle;
             return vectors;
@@ -199,11 +270,14 @@ namespace Intellectika
         public void CreatePlane()
         {
 
+
+
+
             for (int y = 0; y < StepY; y++)
             {
                 for (int x = 0; x < StepX; x++)
                 {
-                    SetPlaneSingle(x , y);
+                    SetPlaneSingle(x, y);
 
                     CreatePlaneTriangels(x);
 
@@ -212,9 +286,11 @@ namespace Intellectika
                 SetFoundationVertices(y);
 
 
-                VerticeIndex += StepX + 1;
+                VerticeIndex += StepX+ 1 ;
             }
             VerticeIndex += StepX + 1;
+
+            
         } // создаёт плоскость
 
         //void UpdateMesh()
@@ -321,74 +397,165 @@ namespace Intellectika
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         ///
 
-        public void CreateTopForSphere() ///создаёт треугольники для верха
+        public void CreateTopForSphere1() ///создаёт треугольники для верха
         {
-            int point = 0;
-            int counter = 0;
-
-                    for (int x = 0; x < StepX; x++)
-                    {
-                     point = VerticeIndex + 1;
-                     Vertices[point] = new Vector3(Radius - counter - x -1 , Height, Radius - counter - z - 1);
-                        VerticeIndex++;
-
-                       Triangels[TriangleIndex] = top[counter];
-                        Triangels[TriangleIndex + 1] = point;
-                        Triangels[TriangleIndex + 2] = top[counter + 1];
-                        TriangleIndex += 3;
-                    }
-                    counter++;
-
-                for (int z = 1; z < StepX; z++)
-                 {
-                point = VerticeIndex + 1;
-                Vertices[point] = new Vector3(Radius - counter - x - 1, Height, Radius - counter - z - 1);
-                VerticeIndex++;
-
-                Triangels[TriangleIndex] = top[counter];
-                Triangels[TriangleIndex + 1] = point;
-                Triangels[TriangleIndex + 2] = top[counter + 1];
-                TriangleIndex += 3;
-                }
-                counter++;
-
-                for (int x = StepX; x > 0; x--)
+            int[] indexes = new int[999];
+            planevectors.v00 = Vertices[top[0]];
+            planevectors.v10 = Vertices[top[StepX]];
+            planevectors.v01 = Vertices[top[StepX * 3]];
+            planevectors.v11 = new Vector3(planevectors.v01.x, Height, planevectors.v10.z);
+            for (int z = 0; z < StepX  ; z++)
+            {
+                for (int x = 0; x < StepX  ; x++)
                 {
-                point = VerticeIndex + 1;
-                Vertices[point] = new Vector3(Radius - counter - x - 1, Height, Radius - counter - z - 1);
-                VerticeIndex++;
+                    Vector3 v00 = new Vector3(Vector3.Lerp(planevectors.v00, planevectors.v01, (float)(x) / StepX).x,
+                    Height,
+                    Vector3.Lerp(planevectors.v00, planevectors.v10, (float)(z) / StepX).z);
+                    Vertices[x + VerticeIndex] = v00;
+                    Vector3 v01 = new Vector3(Vector3.Lerp(planevectors.v00, planevectors.v01, (float)(x + 1) / StepX).x, Height, v00.z);
+                    Vertices[x + VerticeIndex + 1] = v01;
+                    Vector3 v10 = new Vector3(Vector3.Lerp(planevectors.v00, planevectors.v01, (float)(x) / StepX).x, Height,
+                        Vector3.Lerp(planevectors.v00, planevectors.v10, (float)(z + 1) / StepX).z);
+                    Vertices[x + VerticeIndex + StepX + 1] = v10;
+                    Vector3 v11 = new Vector3(Vector3.Lerp(planevectors.v00, planevectors.v01, (float)(x + 1) / StepX).x, 
+                        Height, 
+                        Vector3.Lerp(planevectors.v00, planevectors.v10, (float)(z + 1) / StepX).z);
+                    Vertices[x + VerticeIndex + StepX + 2] = v11;
 
-                Triangels[TriangleIndex] = top[counter];
-                Triangels[TriangleIndex + 1] = point;
-                Triangels[TriangleIndex + 2] = top[counter + 1];
-                TriangleIndex += 3;
+
+                    int vv00 = x + VerticeIndex;
+                    int vv01 = x + VerticeIndex + 1;
+                    int vv10 = x + VerticeIndex + StepX + 1;
+                    int vv11 = x + VerticeIndex + StepX + 2;
+                    Triangels[TriangleIndex] = vv00;
+                    Triangels[TriangleIndex + 1] = vv01;
+                    Triangels[TriangleIndex + 2] = vv11;
+                    Triangels[TriangleIndex + 3] = vv11;
+                    Triangels[TriangleIndex + 4] = vv10;
+                    Triangels[TriangleIndex + 5] = vv00;
+
+                    TriangleIndex += 6;
+
                 }
-                counter++;
+                VerticeIndex += StepX + 1;
+            }
 
-                for (int x = 0; x < StepX; x++)
+
+        }
+
+
+        public void CreateTopForSphere2() ///создаёт треугольники для верха
+        {
+            int[] indexes = new int[999];
+            planevectors.v00 = Vertices[top[0]];
+            planevectors.v10 = Vertices[top[StepX]];
+            planevectors.v01 = Vertices[top[StepX * 3]];
+            planevectors.v11 = new Vector3(planevectors.v01.x, Height, planevectors.v10.z);
+            float plusradius = (float)Radius / (float)(PlaneCount);
+            float currentradius = 0;
+
+            for (int z = 0; z < PlaneCount; z++)
+            {
+                for (int x = 0; x < StepX * PlaneCount; x++)
                 {
-                point = VerticeIndex + 1;
-                Vertices[point] = new Vector3(Radius - counter - x - 1, Height, Radius - counter - z - 1);
-                VerticeIndex++;
 
-                Triangels[TriangleIndex] = top[counter];
-                Triangels[TriangleIndex + 1] = point;
-                Triangels[TriangleIndex + 2] = top[counter + 1];
-                TriangleIndex += 3;
+                    CurrentAngleX += SingleAngle;
+
+                    Vertices[x + VerticeIndex] = planevectors.v00;
+                    Vertices[x + VerticeIndex + StepX + 1] = planevectors.v01;
+                    Vertices[x + VerticeIndex + 1] = planevectors.v10;
+                    Vertices[x + VerticeIndex + StepX + 2] = planevectors.v11;
+
+
+                    int vv00 = x + VerticeIndex;
+                    int vv01 = x + VerticeIndex + StepX + 1;
+                    int vv10 = x + VerticeIndex + 1;
+                    int vv11 = x + VerticeIndex + StepX + 2;
+                    Triangels[TriangleIndex] = vv00;
+                    Triangels[TriangleIndex + 1] = vv01;
+                    Triangels[TriangleIndex + 2] = vv11;
+                    Triangels[TriangleIndex + 3] = vv11;
+                    Triangels[TriangleIndex + 4] = vv10;
+                    Triangels[TriangleIndex + 5] = vv00;
+
+
+                    planevectors.v00 = new Vector3((Radius - currentradius) * geometry.cos(CurrentAngleX - SingleAngle / 2), planevectors.v00.y, (Radius - currentradius) * geometry.sin(CurrentAngleX - SingleAngle / 2));
+                    planevectors.v01 = new Vector3(planevectors.v00.x, planevectors.v01.y, planevectors.v00.z);
+                    planevectors.v10 = new Vector3((Radius - currentradius) * geometry.cos(CurrentAngleX + SingleAngle / 2), planevectors.v00.y, (Radius - currentradius) * geometry.sin(CurrentAngleX + SingleAngle / 2));
+                    planevectors.v11 = new Vector3(planevectors.v10.x, planevectors.v01.y, planevectors.v10.z);
+
+
+
+
+                    TriangleIndex += 6;
+
                 }
-                counter++;
+                currentradius += plusradius;
+                VerticeIndex += StepX + 1;
 
-
+            }
 
 
 
         }
 
 
+
+        public void CreateTopForSphere() ///создаёт треугольники для верха
+        {
+
+            float plusradius = (float)Radius / (float)(PlaneCount);
+            float currentradius = plusradius *2;
+            planevectors.v10 = Vertices[top[StepX]];
+            planevectors.v11 = new Vector3(Vertices[top[StepX * 3]].x, Height, Vertices[top[StepX]].z);
+
+            float SingleAngleForSphere = (float)(360/(StepX * PlaneCount) + 1);
+
+            for (int i = 0; i < PlaneCount - 1; i++)
+            {
+                for (int x = 0; x < StepX ; x++)
+                {
+
+                    planevectors.v00 = Vertices[top[x + i * StepX]];
+                    planevectors.v01 = Vertices[top[x + i * StepX + 1]];
+
+                    planevectors.v10 = new Vector3((Radius - currentradius) * geometry.cos(CurrentAngleX - SingleAngle / 2), planevectors.v10.y, (Radius - currentradius) * geometry.sin(CurrentAngleX - SingleAngle / 2));
+                    planevectors.v11 = new Vector3((Radius - currentradius) * geometry.cos(CurrentAngleX + SingleAngle / 2), planevectors.v10.y, (Radius - currentradius) * geometry.sin(CurrentAngleX + SingleAngle / 2));
+
+                    CurrentAngleX += SingleAngleForSphere;
+
+                    Vertices[x + VerticeIndex + 1] = planevectors.v10;
+                    Vertices[x + VerticeIndex + 2] = planevectors.v11;
+
+                    int vv00 = top[x + i * StepX];
+                    int vv01 = top[x + i * StepX + 1];
+                    int vv10 = x + VerticeIndex;
+                    int vv11 = x + VerticeIndex + 1;
+                    Triangels[TriangleIndex] = vv00;
+                    Triangels[TriangleIndex + 1] = Triangels[TriangleIndex + 4] = vv10;
+                    Triangels[TriangleIndex + 2] = Triangels[TriangleIndex + 3] = vv01;
+                    Triangels[TriangleIndex + 5] = vv11;
+                    TriangleIndex += 6;
+
+
+
+
+                }
+                VerticeIndex += StepX;
+            }
+
+                
+                
+
+            
+
+
+
+        }
         public void SetPlaneSingleTop(int CurrentX, int CurrentZ)
         {
             planevectors.v10 = center - planevectors.v10;
-              planevectors.v01 = center - planevectors.v01;
+            planevectors.v01 = center - planevectors.v01;
             planevectors.v11 = center - planevectors.v11;
             Vector3 v00 = new Vector3(Vector3.Lerp(planevectors.v00, planevectors.v10, (float)(CurrentX) / StepX).x,
             Height,
@@ -396,7 +563,7 @@ namespace Intellectika
             Vertices[CurrentX + VerticeIndex] = v00;
 
 
-            Vector3 v01 = new Vector3(v00.x, Height, Vector3.Lerp(planevectors.v00, planevectors.v01, (float)(CurrentZ+1) / StepX).z);
+            Vector3 v01 = new Vector3(v00.x, Height, Vector3.Lerp(planevectors.v00, planevectors.v01, (float)(CurrentZ + 1) / StepX).z);
             Vertices[CurrentX + VerticeIndex + StepX + 1] = v01;
 
 
@@ -409,4 +576,5 @@ namespace Intellectika
             Vertices[CurrentX + VerticeIndex + StepX + 2] = v11;
         }
     }
+  
 }
--- Assets/Scripts/MeshGeneration/Sphere.cs
+++ Assets/Scripts/MeshGeneration/Sphere.cs
@@ -17,15 +17,18 @@ namespace Intellectika
         {
 
 
-            for (int i = 0; i < PlaneCount; i++)
+            for (int i = 0; i < PlaneCount - 1; i++)
             {
                 planevectors = RecalculatePlaneVectors();
 
                 CreatePlane();
             }
+            planevectors = RecalculatePlaneVectors();
+            CreateLast();
+
             //  CreateBottom();
-            CreateTopForSphere();
-            int counter = 0;
+             CreateTopForSphere();
+            //int counter = 0;
             //foreach (Vector3 i in Vertices)
             //{
             //    Vertices[counter] = i.normalized * Radius;
@@ -34,7 +37,8 @@ namespace Intellectika
             mesh.Clear();
             mesh.vertices = Vertices;
             mesh.triangles = Triangels;
-
+          //  mesh.RecalculateNormals();
+            
 
 
 
@@ -46,6 +50,20 @@ namespace Intellectika
             {
                 Gizmos.DrawSphere(i, 0.25f);
             }
+
+            //foreach (int i in top)
+            //{
+            //    Gizmos.DrawSphere(Vertices[i], 0.25f);
+            //}
+
+            //foreach (int i in bottom)
+            //{
+            //    Gizmos.DrawSphere(Vertices[i], 0.25f);
+            //}
+
+            //Gizmos.DrawSphere(Vertices[top[0]], 0.25f);
+
+            //Gizmos.DrawSphere(Vertices[top[StepX]], 0.25f);
         }
     }
 }
